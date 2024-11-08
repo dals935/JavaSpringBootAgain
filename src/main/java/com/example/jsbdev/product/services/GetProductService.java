ac7f6f8dc6ff5d0b1cbe577a3efe13f34984563f
John Ryan Andal <john.andal@umtc.com.ph>
2024-11-08 10:15:04 +0800

    Added Logging

--- src/main/java/com/example/jsbdev/product/services/GetProductService.java
+++ src/main/java/com/example/jsbdev/product/services/GetProductService.java
@@ -5,6 +5,8 @@ import com.example.jsbdev.product.ProductRepository;
 import com.example.jsbdev.product.Query;
 import com.example.jsbdev.product.model.Product;
 import com.example.jsbdev.product.model.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,15 @@ import java.util.Optional;
 public class GetProductService implements Query<Integer, ProductDTO> {
     private final ProductRepository productRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(GetProductService.class);
+
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public ResponseEntity<ProductDTO> execute(Integer input) {
+        logger.info("Executing " + getClass() + " input: " + input);
         Optional<Product> productOptional = productRepository.findById(input);
         if(productOptional.isPresent()){
             return  ResponseEntity.ok(new ProductDTO(productOptional.get()));
